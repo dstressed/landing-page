7ae8ee9b3652cbf6cbfaaa5b4d186d0dab857125
Dmitry Strezh <36566007+dstressed@users.noreply.github.com>
2018-06-19 23:54:14 +0300

    Fix image extentions

--- js/main.js
+++ js/main.js
@@ -13,12 +13,12 @@ class Slider {
     _switch(event) {
         if (event.target.classList.contains('button-left')) {
             this._i = (this._i - 1 < 0) ? 3 : this._i - 1;
-            this._image.src = `images/screenshot-0${(this._i)}.png`;
+            this._image.src = `images/screenshot-0${(this._i)}.jpg`;
         }
 
         if (event.target.classList.contains('button-right')) {
             this._i = (this._i + 1 > 3) ? 0 : this._i + 1;
-            this._image.src = `images/screenshot-0${(this._i)}.png`;
+            this._image.src = `images/screenshot-0${(this._i)}.jpg`;
         }
     }
 }
